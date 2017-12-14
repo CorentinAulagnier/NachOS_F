9f9fdc42e67924b8f62f4760a4d15c6130eb7bac
corentin <corentinaulagnier@hotmail.fr>
2017-12-14 15:21:13 +0100

    Ajout user prog test/putString.c

---
+++ code/test/putstring.c
@@ -0,0 +1,13 @@
+#include "syscall.h"
+
+void print(char* c) {
+    PutString(c);
+    PutString("\n");
+}
+
+int main() {
+    print("abc");
+    print("defgh");
+    print("coucoucoucou");
+    Halt();
+}
