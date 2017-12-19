d95f20a675429b5e3b1433bc1b28f23cf6f9ad1d
TheSpyGeek <mathieurire@gmail.com>
2017-12-19 09:22:50 +0100

    debut des semaphore putchar...

--- code/userprog/synchconsole.cc
+++ code/userprog/synchconsole.cc
@@ -7,6 +7,10 @@
 
 static Semaphore *readAvail;
 static Semaphore *writeDone;
+static Semaphore *synchChar;
+static Semaphore *synchChar2;
+static Semaphore *synchString;
+static Semaphore *synchInt;
 static void ReadAvail(int arg) { readAvail->V(); }
 static void WriteDone(int arg) { writeDone->V(); }
 
@@ -14,60 +18,91 @@ SynchConsole::SynchConsole(char *readFile, char *writeFile) {
 
     readAvail = new Semaphore("read avail", 0);
     writeDone = new Semaphore("write done", 0);
+    synchChar = new Semaphore("synchChar", 1);
+    synchChar2 = new Semaphore("synchChar2", 1);
+    synchString = new Semaphore("synchString", 1);
+    synchInt = new Semaphore("synchInt", 1);
     console = new Console(readFile, writeFile, ReadAvail, WriteDone, 0);
 }
 
 
 SynchConsole::~SynchConsole() {
-    delete console;
-    delete writeDone;
     delete readAvail;
+    delete writeDone;
+    delete synchChar;
+    delete synchChar2;
+    delete synchString;
+    delete synchInt;
+    delete console;
 }
 
 void SynchConsole::SynchPutChar(const char ch) {
-
+    synchChar->P();
+printf("Apres prise du verrou\n");
     console->PutChar(ch);
+printf("av prise 2nd ver\n");
     writeDone->P();	// wait for write to finish
-      
+printf("aPPPP prise 2nd ver\n");
+    synchChar->V();
+printf("Apres lache du verrou\n");
 }
 
 char SynchConsole::SynchGetChar() {
+printf("000000000000000\n");
+    synchChar2->P();
 
     readAvail->P();	// wait for character to arrive
-    return console->GetChar();
-    
+    char c = console->GetChar();
+printf("AAAAAAAAAAA\n");
+    synchChar2->V();
+printf("BBBBBBBBBBB\n");
+    return c;
+
 
 }
 
 void SynchConsole::SynchPutString(const char s[]) {
-    for(unsigned int i=0; s[i] != '\0'; i++)
+    synchString->P();
+
+    for(unsigned int i=0; s[i] != '\0'; i++){
         SynchPutChar(s[i]);
 
+    }
+    synchString->V();
 }
 
 
 void SynchConsole::SynchGetString(char *s, int n) {
+    synchString->P();
+
     int i;
     for(i = 0; i<n; i++) {
         s[i] = SynchGetChar();
         if(s[i] == '\n') break;
     }
     s[i] = '\0';
+
+    synchString->V();
 }
 
 void SynchConsole::SynchPutInt(int n){
+    synchInt->P();
 
-      char buff[MAX_INT_SIZE];
-      snprintf(buff, MAX_INT_SIZE, "%d", n);
-      SynchPutString(buff);
+    char buff[MAX_INT_SIZE];
+    snprintf(buff, MAX_INT_SIZE, "%d", n);
+    SynchPutString(buff);
 
+    synchInt->V();
 }
 
 int SynchConsole::SynchGetInt(){
-      int n = 0;
-      char buff[MAX_INT_SIZE];
-      SynchGetString(buff, MAX_INT_SIZE);
-      sscanf(buff, "%d", &n);
-      return n;
+    synchInt->P();
 
+    int n = 0;
+    char buff[MAX_INT_SIZE];
+    SynchGetString(buff, MAX_INT_SIZE);
+    sscanf(buff, "%d", &n);
+
+    synchInt->V();
+    return n;
 }
