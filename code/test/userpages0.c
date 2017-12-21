7b8edca656bb7644bb77420324342e3f48c2ba77
mat <mobagle@gmail.com>
2017-12-21 14:13:50 +0100

    fichier test etape 4

---
+++ code/test/userpages0.c
@@ -0,0 +1,21 @@
+#include "syscall.h"
+
+void f1(void* c) {
+    int i=0;
+    char ch = *(char*)c;
+    while(i<10) {
+        PutChar(ch);
+        i++;
+    }
+    UserThreadExit();
+}
+
+int main() {
+    char c1 = 'A', c2 = 'B';
+    int t1 = UserThreadCreate(&f1, &c1);
+    int t2 = UserThreadCreate(&f1, &c2);
+
+    UserThreadJoin(t1);
+    UserThreadJoin(t2);
+    Halt();
+}
