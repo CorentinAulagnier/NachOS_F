da377c129388fa35929bb856bc1720e730135f52
corentin <corentinaulagnier@hotmail.fr>
2018-01-15 09:35:00 +0100

    Ajout commentaire et maj MAXREEMISSIONS

--- code/network/transport.h
+++ code/network/transport.h
@@ -9,8 +9,15 @@ class Transport {
         Transport(float reemission);
         ~Transport();
 
-        // return 0 fail, 1 success
+        /* Envoie un message content de taille sizeContent a la machine to
+         * retourne 0 si cela ne marche pas, retourne 1 en cas de succes
+         */
         bool send(int to, void* content, int sizeContent);
+        
+        /* Attend la reception d un message de la part de from
+         * et l'enregistre dans content
+         * retourne 0 si cela ne marche pas, retourne 1 en cas de succes
+         */
         bool receive(int from, void* content);
         
         
@@ -18,13 +25,55 @@ class Transport {
       //  Semaphore reception;
 };
 
+
+/*****************************************************************/
+/* SEND                                                          */
+/*****************************************************************/
+
+/* Cree les PacketHeader, et MailHeader
+ * et lance sendingLoop
+ * retourne 0 si cela ne marche pas, retourne 1 en cas de succes
+ */
 bool trySend(int to, int numPaquet, void* content, int sizeMail);
+
+/* Envoie en boucle tant que !ackReceive()
+ * retourne 0 si cela ne marche pas, retourne 1 en cas de succes
+ */
 bool sendingLoop(PacketHeader outPktHdr, MailHeader outMailHdr, void* content);
+
+/* Cherche l ACK correspondant dans la boite mail
+ * retourne 1 si l ACK est trouvé, retourne 0 sinon
+ */
 bool ackReceive(int fromMachine, int numPaquet);
 
+
+/*****************************************************************/
+/* RECEIVE                                                       */
+/*****************************************************************/
+
+/* Cree les PacketHeader, et MailHeader
+ * et lance sendingAckLoop
+ * retourne 0 si cela ne marche pas, retourne 1 en cas de succes
+ */
 bool tryAck(int to, int numPaquet, bool lastAck);
+
+/* Envoie en boucle tant que !nextReceive()
+ * retourne 0 si cela ne marche pas, retourne 1 en cas de succes
+ */
 bool sendingAckLoop(PacketHeader outPktHdr, MailHeader outMailHdr, bool lastAck);
+
+/* Cherche si le prochain message est dans la boite mail
+ * retourne 1 si l ACK est trouvé, retourne 0 sinon
+ */
 bool nextReceive(int fromMachine, int numPaquet);
 
+
+/*****************************************************************/
+/* TOOLS                                                         */
+/*****************************************************************/
+
+/* Cree les PacketHeader et MailHeader correspondant au parametres
+ */
 PacketHeader creerPacketHeader(int to, int from);
 MailHeader creerMailHeader(int numBoxTo, int numBoxFrom, int numPaquet, int size);
+
